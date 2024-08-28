6535df27ce78a92e4230b09d307d57892478679c
Khalil Najjar <knajjars@gmail.com>
2024-08-28 09:55:09 +0200

    add features

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,31 @@ const baasProvider = defineCollection({
       name: z.string(),
       description: z.string(),
       logo: image(),
+      sdks: z.array(z.string()),
+      features: z.object({
+        authentication: z
+          .object({
+            social: z.boolean(),
+            email: z.boolean(),
+            phone: z.boolean(),
+            magicLinks: z.boolean(),
+            saml: z.boolean(),
+            oidc: z.boolean(),
+            userManagement: z.boolean(),
+            freeMonthlyActiveUsers: z.number(),
+            pricePerExtraActiveUser: z.number(),
+          })
+          .optional(),
+        database: z.object({}).optional(),
+        realtime: z.object({}).optional(),
+        functions: z.object({}).optional(),
+        selfHosted: z.object({}).optional(),
+        storage: z.object({}).optional(),
+        vectorDatabase: z.object({}).optional(),
+        scheduledJobs: z.object({}).optional(),
+        pushNotifications: z.object({}).optional(),
+        permissions: z.object({}).optional(),
+      }),
     }),
 });
 
