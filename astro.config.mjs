b35592c5ef5ea23c8ae1cf89221feeec1ce59e94
Khalil Najjar <knajjars@gmail.com>
2024-08-29 10:17:28 +0200

    progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stackchoice.dev",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    icon({ include: { mdi: ["*"] } }),
+  ],
 });
